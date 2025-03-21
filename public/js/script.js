5f15dded478a32d35e068e81853b209c1daa8513
Kuengatenzin-yoo <kuengatenzin2005@gmail.com>
2025-03-21 17:35:38 +0600

    Practical 2

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("JavaScript Loaded!");
+const express = require('express');
+const router = express.Router();
+// Render the landing page
+router.get('/', (req, res) => {
+res.render('index', { title: 'Online Book Store' });
 });
+module.exports = router;
